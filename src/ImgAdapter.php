1a2e372839dac7e2900a35987e0c370ef6745678
Jordy Manner <jordy.manner@milkcreation.fr>
2021-02-19 19:26:07 +0000

    filesystem >> master

---
+++ src/ImgAdapter.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\Filesystem;
+
+class ImgAdapter extends LocalAdapter implements ImgAdapterInterface
+{
+}
---
+++ src/ImgFilesystemInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\Filesystem;
+
+interface ImgFilesystemInterface extends LocalFilesystemInterface
+{
+    /**
+     * Récupération de la source d'une image encodée en base64.
+     *
+     * @param string $path Chemin relatif vers la ressource image.
+     *
+     * @return string|null
+     */
+    public function src(string $path): ?string;
+
+    /**
+     * Récupération du rendu d'affichage d'une image.
+     *
+     * @param string $path Chemin relatif vers la ressource image.
+     * @param array|null $attrs Liste des attributs de balise HTML. Si null, pas d'encapsulation (.svg uniquement).
+     *
+     * @return string
+     */
+    public function render(string $path, ?array $attrs = []): ?string;
+}
